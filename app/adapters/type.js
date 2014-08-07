6aecddea72efb0ff538665705b145d7a2afdc104
Karim Baaba <karim.baaba@gmail.com>
2014-08-07 03:14:12 -0600

    Improve store and add simple instances adapter

--- app/adapters/type.js
+++ app/adapters/type.js
@@ -9,54 +9,14 @@ export default Ember.Object.extend({
         var queryExec,
             self = this;
 
-        if (uri === 'all') {
-            queryExec = service.createQueryExecutionStr(this.get('allQuery'));
+        queryExec = service.createQueryExecutionStr(this.get('allQuery'));
 
-            return new Ember.RSVP.Promise(function(resolve, reject) {
-                queryExec.execSelect().then(function(resultSet) {
-                    resolve(this._resultsToArray(resultSet));
-                }.bind(self), function(error) {
-                    reject('Error! Unable to fetch types');
-                });
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            queryExec.execSelect().then(function(resultSet) {
+                resolve(service.resultsToArray(resultSet, 'type'));
+            }, function(error) {
+                reject('Error! Unable to fetch types');
             });
-        }
-    },
-
-    // TODO: Move this elsewhere. Other adapters will probably need this.
-    _resultsToArray: function(resultSet) {
-        var row, entry, uri, label, lang,
-            map = {};
-
-        var setLabel = function(entry, lang, label) {
-            if (lang) {
-                entry[lang] = label;
-            } else {
-                entry['default'] = label;
-            }
-        };
-
-        while (resultSet.hasNext()) {
-            row   = resultSet.nextBinding();
-            uri   = row.varNameToEntry.type.node.uri;
-            label = row.varNameToEntry.label.node.literalLabel.val;
-            lang  = row.varNameToEntry.label.node.literalLabel.lang;
-
-            if ((entry = map[uri])) {
-                setLabel(entry, lang, label);
-            } else {
-                entry = {};
-                setLabel(entry, lang, label);
-                map[uri] = entry;
-            }
-        }
-        // Types is an ArrayController
-        var arr = Ember.A();
-
-        _.each(map, function(value, key, list) {
-            arr.pushObject({ uri: key, labels: value});
         });
-
-        return arr;
-    },
-
+    }
 });
--- app/models/store.js
+++ app/models/store.js
@@ -31,7 +31,48 @@ export default Ember.Object.extend({
         if (adapter.get(method)) {
             return adapter[method](this.get('endpoint'), params);
         } else {
-            return Ember.RSVP.Promise.reject('Error! Invalid query type');
+            return Ember.RSVP.reject('Error! Invalid query');
         }
+    },
+
+    init: function() {
+        // this adds another method to Jass'a sparql service class
+        // makes it easier for our adapter to parse results
+        Jassa.service.SparqlServiceHttp.addMethods({
+            resultsToArray: function(resultSet, uriVar) {
+                var row, entry, uri, label, lang,
+                    map = {};
+
+                var setLabel = function(entry, lang, label) {
+                    if (lang) {
+                        entry[lang] = label;
+                    } else {
+                        entry['default'] = label;
+                    }
+                };
+
+                while (resultSet.hasNext()) {
+                    row   = resultSet.nextBinding();
+                    uri   = row.varNameToEntry[uriVar].node.uri;
+                    label = row.varNameToEntry.label.node.literalLabel.val;
+                    lang  = row.varNameToEntry.label.node.literalLabel.lang;
+
+                    if ((entry = map[uri])) {
+                        setLabel(entry, lang, label);
+                    } else {
+                        entry = {};
+                        setLabel(entry, lang, label);
+                        map[uri] = entry;
+                    }
+                }
+                var arr = Ember.A();
+
+                _.each(map, function(value, key, list) {
+                    arr.pushObject({ uri: key, labels: value});
+                });
+
+                return arr;
+            }
+        });
     }
 });
