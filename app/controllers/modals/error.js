6ad32e1502c2f436d0791088eb9a44398db43340
Karim Baaba <karim.baaba@gmail.com>
2014-08-07 03:52:20 -0600

    Add a loading substate and fix errors not displaying on page reload

--- app/controllers/modals/error.js
+++ app/controllers/modals/error.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         close: function() {
-            this.transitionToRoute('application');
             return this.send('closeModal', 'error');
         }
     }
--- app/routes/missing.js
+++ app/routes/missing.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    activate: function() {
+    redirect: function() {
+        // modal always needs something to render ontop of
+        this.intermediateTransitionTo('application');
         this.send('openModal', 'error', 'Page Not Found!');
     }
 });
