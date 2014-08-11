d968124c245d4a2566a5e0f09bc9e73cfcd2b166
Karim Baaba <karim.baaba@gmail.com>
2014-08-11 00:09:39 -0600

    Use Selectionic with resources-list and add comment lookup in resource-item

--- app/components/resource-item.js
+++ app/components/resource-item.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
+import Notify from 'ember-notify';
 
+// this component is for rendering list items and displaying comments
+// no selection logic happens here, all in the parent component
 export default Ember.Component.extend({
+    classNames: ['list-group-item'],
+    attributeBindings: ['uri'],
+    tagName: 'li',
+
+    toggleComment: function() {
+        this.toggleProperty('showingComment');
+    }.observes('comments'),
+
+    toggleFocus: function() {
+        this.toggleProperty('focused');
+    },
+
+    // this should also be achieved by using .on('mouseEnter mouseLeave')
+    // but that's currently not working, will revisit after beta
+    mouseEnter: Ember.aliasMethod('toggleFocus'),
+    mouseLeave: Ember.aliasMethod('toggleFocus'),
+
+    actions: {
+        showComment: function() {
+            var comments = this.get('comments');
+
+            if (comments && !_(comments).isEmpty()) { return this.toggleComment(); }
+
+            var uri = this.get('uri');
+
+            this.store.commentFor(uri).then(function(comment) {
+                if (_(comment).isEmpty()) {
+                    this.set('comments', { default: 'This resource has no description' });
+                } else {
+                    this.set('comments', comment);
+                }
+            }.bind(this), function(error) {
+                Notify.error('Unable to fetch comment for ' + uri, { closeAfter: 5000 });
+            });
+        }
+    }
 });
--- app/components/resources-list.js
+++ app/components/resources-list.js
@@ -1,6 +1,56 @@
 import Ember from 'ember';
 
+// The following component combines jquery's selectonic plugin
+// with bootstrap's list-group to support multi and keyboard selections
 export default Ember.Component.extend({
     tagName: 'ul',
     classNames: ['list-group'],
-});
+
+    initList: function() {
+        var self = this;
+
+        this.set('selectedItems', Ember.A());
+        this.$().selectonic({
+            multi: false,
+            keyboard: true,
+            // for Bootstrap
+            selectedClass: 'active',
+            // To avoid selecting virtual views script tags
+            filter: '> li',
+            listClass: 'list-group-item',
+            // Event calbacks
+            select      : function(event, ui) { Ember.run(self, self.select, ui.items); },
+            unselect    : function(event, ui) { Ember.run(self, self.unselect, ui.items); },
+        });
+    }.on('didInsertElement'),
+
+    updateURL: function() {
+        var items = this.get('selectedItems');
+
+        if (items.length === 0) { return; }
+
+        if (items.length === 1) {
+            this.sendAction('transition', this.get('toRoute'),
+                            'all', items.get('firstObject'));
+
+        } else {
+            // TODO: multiple selections
+        }
+    }.observes('selectedItems.[]'),
+
+    select: function(items) {
+        _(items).each(function(element) {
+            this.get('selectedItems').pushObject(this.getItemURI(element));
+        }, this);
+    },
+
+    unselect: function(items) {
+        _(items).each(function(element) {
+            this.get('selectedItems').removeObject(this.getItemURI(element));
+        }, this);
+    },
+
+    getItemURI: function(item) {
+        return this.$(item).attr('uri');
+    }
+ });
