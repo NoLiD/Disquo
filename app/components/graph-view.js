f6fef7dc0e8efec07db2fa88d0da528834e3392e
kdbanman <kdbanman@ualberta.ca>
2014-10-30 06:26:17 -0600

    mark locations for full version of predicate data.

--- app/components/graph-view.js
+++ app/components/graph-view.js
@@ -6,8 +6,13 @@ export default Ember.Component.extend({
 
   //this observes the resources list and is invoked on initialization
   draw: function () {
-    //resources is a list of 'resource' objects from modesl/resource
-    console.log(this.get('resources'));
+    //TODO:
+    //resources is a currently list of 'resource' objects from models/resource
+    //see adapters/predicate.js for query execution
+    this.get('resources').forEach(function (res, i) {
+        //console.log(i);
+        //console.log(res.get('label'));
+    });
 
   }.observes('resources')
 });
