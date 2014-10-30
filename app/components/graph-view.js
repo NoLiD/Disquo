dac1bfe976180e5670ba910199f938d8ff14028c
kdbanman <kdbanman@ualberta.ca>
2014-10-30 08:15:50 -0600

    define fact and feature views

--- app/components/graph-view.js
+++ app/components/graph-view.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
+// this component may not be fully reusable between both graph views:
+//
+// 1. feature view
+//      triggered by 1 or more thing resources selected
+//      multiple thing resources selected, multiple predicates shown as nodes
+//
+// 2. fact view 
+//      triggered by single predicate (node) selected from feature view
+//      shows multiple thing resources, single predicate, multiple values
+// 
+// Both 1 and 2 render a graph with unlabeled, directed edges centered around a
+// set of selected thing resource nodes.
+
 // Peek at http://jsbin.com/d3ember-barchart/13/edit
 export default Ember.Component.extend({
   classNames: ['panel-body'],
@@ -8,8 +21,14 @@ export default Ember.Component.extend({
   draw: function () {
     //TODO:
     //resources is a currently list of 'resource' objects from models/resource
-    //see adapters/predicate.js for query execution
+    //see adapters/predicate.js for query and query execution
+    
     this.get('resources').forEach(function (res, i) {
+        // each object is for one of the selected items (thing resources)
+        
+        // this component should not care whether or not only predicates shared
+        // by each of the selected resources are shown
+
         //console.log(i);
         //console.log(res.get('label'));
     });
