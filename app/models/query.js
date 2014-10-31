b9585d59a9ad5ef71636688c1f2650cb1f2a3359
Karim Baaba <karim.baaba@gmail.com>
2014-10-30 22:13:38 -0600

    Add a query interface via handlebars templates

---
+++ app/models/query.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  PAGE_SIZE: 100,
+  OFFSET: 0,
+
+  rdfRefs: {
+    label:'<http://www.w3.org/2000/01/rdf-schema#label>',
+    comment:'<http://www.w3.org/2000/01/rdf-schema#comment>'
+  },
+
+  template: function() {
+    return Handlebars.compile(this.get('query'), {noEscape: true});
+  }.property('query'),
+
+  result: function(context) {
+    var template = this.get('template');
+    var rdfRefs  = this.get('rdfRefs');
+
+    if (context !== undefined) {
+      return template(Ember.$.extend(context, rdfRefs));
+    } else {
+      return template(rdfRefs);
+    }
+  }
+});
