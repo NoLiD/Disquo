c5d90ff50eb9c0d7eb8a402f563ac4b2ea3fa76c
Karim Baaba <karim.baaba@gmail.com>
2014-10-30 22:12:34 -0600

    Add missing mock resource test

---
+++ tests/unit/models/resource-test.js
@@ -0,0 +1,15 @@
+import {
+  moduleForModel,
+  test
+} from 'ember-qunit';
+
+moduleFor('model:resource', 'Resource', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function() {
+  var model = this.subject();
+  // var store = this.store();
+  ok(!!model);
+});
