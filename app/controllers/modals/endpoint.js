284c1402d820501c1abeafa863fae3344ffe58d1
Karim Baaba <karim.baaba@gmail.com>
2014-11-04 21:51:28 -0700

    Cleanup endpoint selector modal and remove unnecessary port option

--- app/controllers/modals/endpoint.js
+++ app/controllers/modals/endpoint.js
@@ -4,6 +4,7 @@ import Notify from 'ember-notify';
 export default Ember.Controller.extend({
   needs: ['endpoint'],
   _endpoint: Ember.computed.alias('controllers.endpoint.model.url'),
+  regex: new RegExp('[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)'),
 
   actions: {
     close: function() {
@@ -11,29 +12,16 @@ export default Ember.Controller.extend({
     },
 
     enterEndpoint: function() {
-      var port = this.get('port'),
-          endpoint = this.get('endpoint'),
+      var regex     = this.get('regex'),
+          endpoint  = this.get('endpoint'),
           _endpoint = this.get('_endpoint');
-
-      var regex = new RegExp('[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)');
 
       if (endpoint === undefined || !regex.test(endpoint)) {
         return Notify.error('Invalid Endpoint URL!');
       }
 
-      if (endpoint.lastIndexOf("http://", 0) !== 0) {
-        endpoint =  "http://" + endpoint;
-      }
-
-      if (port === undefined || port === '') {
-        this.set('port', 80);
-        port = 80;
-      } else if (isNaN(parseInt(port))) {
-        return Notify.error('Invalid port!');
-      }
-
       if (endpoint !== _endpoint) {
-        this.transitionToRoute('endpoint', endpoint, port);
+        this.transitionToRoute('endpoint', endpoint);
       }
 
       return this.send('close');
