2a0968b4244399706942686fc984f35cfb4838bc
Karim Baaba <karim.baaba@gmail.com>
2014-11-22 04:03:28 -0700

    All routes everything

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('endpoint', { path: '/:url' }, function() {
+  this.route('endpoint', { path: '/:url' }, function() {
     this.route('types', { path: '/types/:query/:selected' });
     this.route('things', { path: '/things/:query/:selected' });
     this.route('predicates', { path: '/predicates/:query/:selected' });
