3aff82d1fe2f324379adb571fb761f226b6a210c
Karim Baaba <karim.baaba@gmail.com>
2014-12-06 17:35:20 -0700

    Prepare rest of queries

--- app/adapters/thing.js
+++ app/adapters/thing.js
@@ -6,6 +6,18 @@ export default BaseAdapter.extend({
   AllQuery: Query.extend({ template: 'SELECT DISTINCT ?instance ?label WHERE { {{#each selected}} ?instance a <{{this}}> . {{/each}} ?instance {{label}} ?label }',
                            key: {var: 'instance', label: 'label'} }),
 
+  PredsQuery: Query.extend({ template: 'SELECT DISTINCT ?instance ?label WHERE { {{#each selected}} ?instance <{{this}}> [] . {{/each}} ?instance {{label}} ?label }',
+                           key: {var: 'instance', label: 'label'} }),
+
+  PredsObjsQuery: Query.extend({ template: 'SELECT DISTINCT ?instance ?label WHERE { {{#each selected}} ?instance <{{this}}> {{predicate}} . {{/each}} ?instance {{label}} ?label }',
+                           key: {var: 'instance', label: 'label'} }),
+
+  ObjsQuery: Query.extend({ template: 'SELECT DISTINCT ?instance ?label WHERE { {{#each selected}} ?instance [] <{{this}}> . {{/each}} ?instance {{label}} ?label }',
+                           key: {var: 'instance', label: 'label'} }),
+
+  SameTypesQuery: Query.extend({ template: 'SELECT DISTINCT ?instance ?label WHERE { {{#each selected}} <{{this}}> a ?object . {{/each}} ?instance a ?object . ?instance {{label}} ?label }',
+                           key: {var: 'instance', label: 'label'} }),
+
   all: function(selected) {
     var query = this.getOrCreateQuery(this.get('AllQuery'), 'all', selected);
 
@@ -17,5 +29,57 @@ export default BaseAdapter.extend({
                                               selected.toString());
           }
     );
+  },
+
+  withPredicates: function(selected) {
+    var query = this.getOrCreateQuery(this.get('PredsQuery'), 'preds', selected);
+
+    return query.get('result')
+          .then(function(result) {
+            return { things: result };
+          }, function() {
+            return Ember.RSVP.Promise.reject('Unable to fetch things with predicates of ' +
+                                             selected.toString());
+          }
+    );
+  },
+
+  withPredicateAndObjects: function(selected, predicate) {
+    var query = this.getOrCreateQuery(this.get('PredsObjsQuery'), 'predsObjs', selected, predicate);
+
+    return query.get('result')
+          .then(function(result) {
+            return { things: result };
+          }, function() {
+            return Ember.RSVP.Promise.reject('Unable to fetch things with predicate ' +
+                                             predicate + ' and objects of ' + selected.toString());
+          }
+    );
+  },
+
+  withObjects: function(selected) {
+    var query = this.getOrCreateQuery(this.get('ObjsQuery'), 'objs', selected);
+
+    return query.get('result')
+          .then(function(result) {
+            return { things: result };
+          }, function() {
+            return Ember.RSVP.Promise.reject('Unable to fetch things with objects ' +
+                                             selected.toString());
+          }
+    );
+  },
+
+  ofSameTypes: function(selected) {
+    var query = this.getOrCreateQuery(this.get('SameTypesQuery'), 'sameTypes', selected);
+
+    return query.get('result')
+          .then(function(result) {
+            return { things: result };
+          }, function() {
+            return Ember.RSVP.Promise.reject('Unable to fetch things of same types as ' +
+                                             selected.toString());
+          }
+        );
   }
 });
--- app/adapters/type.js
+++ app/adapters/type.js
@@ -6,6 +6,10 @@ export default BaseAdapter.extend({
   AllQuery: Query.extend({ template: 'SELECT DISTINCT ?type ?label WHERE { [] a ?type . ?type {{label}} ?label }',
                            key: {var: 'type', label: 'label'} }),
 
+  typesQuery: Query.extend({ template: 'SELECT DISTINCT ?type ?label WHERE { {{#each selected}} [] {{this}} ?type . {{/each}} ?type {{label}} ?label }',
+                           key: {var: 'type', label: 'label'} }),
+
+
   all: function() {
     var query = this.getOrCreateQuery(this.get('AllQuery'), 'all', 'all');
 
@@ -14,6 +18,19 @@ export default BaseAdapter.extend({
             return { types: result };
           }, function() {
             return Ember.RSVP.Promise.reject('Unable to fetch types of all');
+          }
+    );
+  },
+
+  typesOf: function(selected) {
+    var query = this.getOrCreateQuery(this.get('typesQuery'), 'typesOf', selected);
+
+    return query.get('result')
+          .then(function(result) {
+            return { types: result };
+          }, function() {
+            return Ember.RSVP.Promise.reject('Unable to fetch types of ' +
+                                             selected.toString());
           }
     );
   }
