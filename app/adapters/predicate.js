162a9ef3b506e05bfe2741571b146938f1d9b2e8
kdbanman <kdbanman@ualberta.ca>
2014-12-29 04:04:19 -0700

    fix refresh and div insertion bugs. use better cytoscape container. improve style. scaffold node pool and usage thereof. plan and improve code generally. closes #19. closes #16.

--- app/adapters/predicate.js
+++ app/adapters/predicate.js
@@ -2,27 +2,35 @@ import Ember from 'ember';
 import BaseAdapter from './base-adapter';
 import Query from '../models/queries/async-select';
 
+function reduceToIntersection(selected, results) {
+  // keep predicate in results iff it belongs to each selection
+  var outgoing = results.outgoing;
+  var incoming = results.incoming;
+
+  return { predicates: { outgoing: outgoing,
+                         incoming: incoming } };
+}
+
 export default BaseAdapter.extend({
   Outgoing: Query.extend({ template: 'SELECT DISTINCT ?subject ?predicate ?label WHERE { VALUES ?subject { {{#each selected}} <{{this}}> {{/each}} } ?subject ?predicate [] . ?predicate {{label}} ?label . }',
                            key: {var: 'subject'},
-                           variables: [ {var: 'predicate', label: 'label', mapName: 'outPredicates'} ] }),
+                           variables: [ {var: 'predicate', label: 'label', mapName: 'outer'} ] }),
 
   Incoming: Query.extend({ template: 'SELECT DISTINCT ?object ?predicate ?label WHERE { VALUES ?object { {{#each selected}} <{{this}}> {{/each}} } [] ?predicate ?object . ?predicate {{label}} ?label . }',
                            key: {var: 'object'},
-                           variables: [ {var: 'predicate', label: 'label', mapName: 'inPredicates'} ] }),
+                           variables: [ {var: 'predicate', label: 'label', mapName: 'outer'} ] }),
 
   all: function(selected) {
     var incoming = this.getOrCreateQuery(this.get('Incoming'), 'all.in', selected),
         outgoing = this.getOrCreateQuery(this.get('Outgoing'), 'all.out', selected);
 
     var queries = {
-      outgoing  : outgoing.get('result'),
-      incoming  : incoming.get('result'),
+      outgoing: outgoing.get('result'),
+      incoming: incoming.get('result'),
     };
 
     return Ember.RSVP.hash(queries).then(function(results) {
-      return { predicates: { outgoing: results.outgoing,
-                             incoming: results.incoming } };
+      return reduceToIntersection(selected, results);
     }, function() {
       return Ember.RSVP.Promise.reject('Unable to fetch predicates of ' +
                                         selected.toString());
--- app/components/graph-view.js
+++ app/components/graph-view.js
@@ -14,7 +14,6 @@ import Cyto from 'cytoscape';
 // Both 1 and 2 render a graph with unlabeled, directed edges centered around a
 // set of selected thing resource nodes.
 
-//TODO break graph style and layout options into app/styles/...
 var graphstyle = Cyto.stylesheet()
   .selector('node')
   .css({
@@ -26,9 +25,9 @@ var graphstyle = Cyto.stylesheet()
   .selector('edge')
   .css({
     'target-arrow-shape': 'triangle',
-    'width': 3,
-    'line-color': '#999',
-    'line-style': 'dotted'
+    'width': 1.5,
+    'line-color': '#777',
+    'line-style': 'solid'
   })
   .selector('node.central')
   .css({
@@ -57,15 +56,14 @@ var layoutOptions = {
   name: 'cose',
 
   animate: false, // whether to show the layout as it's running
-  refresh: 10,
   fit: true, // whether to fit the viewport to the graph
   padding: 30, // padding on fit
-  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  boundingBox: undefined, // constrain layout bounds
   random: false, // whether to use random initial positions
   ready: undefined, // callback on layoutready
   stop: undefined, // callback on layoutstop
 
-  nodeRepulsion: 900000,
+  nodeRepulsion: 1200000,
   nodeOverlap: 100,
   idealEdgeLength: 40,
   edgeElasticity: 200,
@@ -76,159 +74,264 @@ var layoutOptions = {
   minTemp: 1.0
 };
 
+var cyOpts = {
+  style: graphstyle,
+
+  layout: layoutOptions,
+
+  motionBlur: false,
+
+  minZoom: 0.4,
+  maxZoom: 4,
+
+  panningEnabled: true,
+  userPanningEnabled: true,
+
+  boxSelectionEnabled: true,
+};
+
 function newNode(uri, label, group) {
+  var fullName = label;
+
+  if (label.length > 16) {
+    label = label.substring(0,14) + '...';
+  }
+
   return {
     classes: group,
     data: {
       id: uri,
-      name: label
-    }
+      name: label,
+      hiddenName: fullName
+    },
+    group: "nodes",
+    position: {x: 100, y: 100}
   };
 }
 
 function newEdge(src, tgt) {
-  return { data: { source: src, target: tgt } };
+  return {
+    data: { source: src, target: tgt },
+    group: "edges"
+  };
 }
 
-// TODO: for predicate view, exclude outer nodes with only one edge
-//       for value view, do not exclude any outer nodes
+function getGraphArrays(results) {
+  var selectMap = results.get('selected'),
+      outMap    = results.get('resources.outgoing'),
+      inMap     = results.get('resources.incoming'),
+      nodeSet   = [],
+      nodes     = [],
+      edges     = [];
+
+  // start node array with central (selected) nodes.
+  selectMap.forEach(function (s) {
+    // set central class for style and click event choice
+    nodes.push(newNode(s.get('uri'), s.get('label'), 'central'));
+    nodeSet.push(s.get('uri'));
+  });
+
+  // add outer nodes (predicates or predicate values) and build edge array
+  outMap.forEach(function (s) {
+    // (outMap holds a subset of selectMap, so no need to add nodes)
+
+    // detect outgoing predicates for this selected resource.
+    var preds = s.get('outer');
+    if (preds) {
+      preds.forEach(function(pred) {
+
+        var uri   = pred.get('uri'),
+            label = pred.get('label');
+
+        // add predicate to node list if it wasn't in the selection
+        if (!nodeSet.contains(uri)) {
+          nodes.push(newNode(uri, label, 'outer'));
+          nodeSet.push(uri);
+        }
+
+        // add edge between from selected resource to predicate
+        edges.push(newEdge(s.get('uri'), uri));
+      });
+    }
+  });
+
+  // yeah! repeating myself!
+  // do the same for inbound predicates, but reverse edge direction
+  inMap.forEach(function (s) {
+    var preds = s.get('outer');
+    if (preds) {
+      preds.forEach(function(pred) {
+        var uri   = pred.get('uri'),
+            label = pred.get('label');
+
+        // add predicate to node list if it wasn't in the selection
+        if (!nodeSet.contains(uri)) {
+          nodes.push(newNode(uri, label, 'outer'));
+          nodeSet.push(uri);
+        }
+
+        // add edge between from predicate to selected resource
+        edges.push(newEdge(uri, s.get('uri')));
+      });
+    }
+  });
+
+  return { nodes: nodes, edges: edges };
+};
+
+var nodePool = {
+  init: function (cy) {
+    this.cy = cy;
+
+    // list of inactive/active nodes
+    this.inactiveNodes = Ember.A();
+    this.activeNodes = Ember.A();
+    // map of inactive/active edge lists, using 'srcid:tgtid' string keys
+    this.inactiveEdges = new Ember.Map();
+    this.activeEdges = new Ember.Map();
+  },
+
+  // useInactiveX returns an element not currently 'restored' from 'removal',
+  // to use the cytoscape api terminology.  must not be called before cytoscape
+  // is initialized.
+  useInactiveNode: function (data) {
+    if (this.inactiveNodes.length === 0) {
+      // node pool is empty - create new node with data
+      // add the node to cytoscape
+      // add node to active pool
+    } else {
+      // remove a node from inactive, move to active, mutate its data
+      // restore the node to cytoscape
+    }
+    // return the element
+  },
+
+  // useInactiveEdge requires source and target node ids because they are
+  // immutable
+  useInactiveEdge: function (srcid, tgtid) {
+    if (this.inactiveEdges.get(srcid + ':' + tgtid).length === 0) {
+      // create the new edge and add it to the active pool
+    } else {
+      // remove edge from inactive, move to active, restore to cytoscape
+    }
+    // return the element
+  },
+
+  deactivateAll: function () {
+    // for each element in inactive pool
+      // move element from active pool to inactive pool
+      // remove element from cytoscape
+  }
+
+};
 
 export default Ember.Component.extend({
   classNames: ['graph'],
 
-  init: function() {
-    this.set('nodeSet', Ember.A());
-    this.set('nodes', Ember.A());
-    this.set('edges', Ember.A());
-  },
-
-  updateSelect: function() {
-    var nodes   = this.get('nodes'),
-        nodeSet = this.get('nodeSet'),
-        select  = this.get('selected');
-
-    // start node array with central (selected) nodes.
-    if (select !== undefined) {
-      select.forEach(function (s) {
-        // set central class for style and click event choice
-        nodes.addObject(newNode(s.get('uri'), s.get('label'), 'central'));
-        nodeSet.addObject(s.get('uri'));
-      });
-
-      this.reRender();
-    }
-  }.observes('selected.@each'),
-
-  updateOut: function() {
-    var nodes    = this.get('nodes'),
-        edges    = this.get('edges'),
-        nodeSet  = this.get('nodeSet'),
-        outbound = this.get('resources.outgoing');
-
-    // add outer nodes (predicates or predicate values) and build edge array
-    if (outbound !== undefined) {
-      outbound.forEach(function (s) {
-        // (outbound holds subset of selectMap, so no need to add nodes)
-
-        // detect outgoing predicates for this selected resource.
-        var preds = s.get('outPredicates');
-        if (preds) {
-          preds.forEach(function(key, value) {
-            var uri   = value.get('uri'),
-                label = value.get('label');
-
-            // add predicate to node list if it wasn't in the selection
-            if (!nodeSet.contains(uri)) {
-              nodes.addObject(newNode(uri, label, 'outer'));
-              nodeSet.addObject(uri);
-            }
-
-            // add edge between from selected resource to predicate
-            edges.addObject(newEdge(s.get('uri'), uri));
-          });
-        }
-      });
-
-      this.reRender();
-    }
-  }.observes('resources.outgoing.size'),
-
-  updateIn: function() {
-    var nodes   = this.get('nodes'),
-        edges   = this.get('edges'),
-        nodeSet = this.get('nodeSet'),
-        inbound = this.get('resources.incoming');
-
-    // do the same for inbound predicates, but reverse edge direction
-    if (inbound !== undefined) {
-      inbound.forEach(function (s) {
-        var preds = s.get('inPredicates');
-        if (preds) {
-          preds.forEach(function(key, value) {
-            var uri   = value.get('uri'),
-                label = value.get('label');
-
-            // add predicate to node list if it wasn't in the selection
-            if (!nodeSet.contains(uri)) {
-              nodes.addObject(newNode(uri, label, 'outer'));
-              nodeSet.addObject(uri);
-            }
-
-            // add edge between from predicate to selected resource
-            edges.addObject(newEdge(uri, s.get('uri')));
-          });
-        }
-      });
-
-      this.reRender();
-    }
-  }.observes('resources.incoming.size'),
-
-  //must not be called before 'didInsertElement'
-  reRender: function() {
-    this.cy.add({
-      nodes: this.get('nodes').toArray(),
-      edges: this.get('edges').toArray()
-    });
-    console.log(this.get('nodes').toArray());
-    this.cy.layout({name: 'grid'});
-    this.cy.forceRender();
-  },
-
-  //this is invoked on initialization
   didInsertElement: function () {
+    var that = this;
+
 
     // initialize cytoscape
-    this.cy = Cyto({  // jshint ignore:line
+    that.$().cytoscape(cyOpts);
 
-      container: this.$()[0],
+    // add cytoscape context to component
+    that.cy = that.$().cytoscape('get');
 
-      style: graphstyle,
-      motionBlur: false,
+    // ensure the graph is rendered on page refresh/direct link
+    that.cy.on('ready', function () {
+      that.draw();
+    });
+  },
 
-      layout: layoutOptions,
+  // must not be called before cytoscape is initialized
+  hideNonintersecting: function() {
+    var expectedDegree = this.get('selected').length;
+    this.cy.elements('node.outer').forEach(function (node) {
+      if (node.degree(false) < expectedDegree) node.hide();
+      else node.show();
+    });
+  },
 
-      panningEnabled: true,
-      userPanningEnabled: true,
+  detectNewSelection: (function () {
+    var current = undefined;
+    var that = this;
+    return function (newSelection) {
+      if (newSelection === current) {
+        return false;
+      } else {
+        console.log('new selection detected');
+        current = newSelection;
+        return true;
+      }
+    };
+  })(),
 
-      boxSelectionEnabled: true,
+  updateSelect: function() {
+    this.detectNewSelection(this.get('selected'));
+    console.log('selection updated');
+  }.observes('selected.@each').on('init'),
+
+  updateOut: function() {
+    this.detectNewSelection(this.get('selected'));
+    console.log('outgoing updated');
+  }.observes('resources.outgoing.size').on('init'),
+
+  updateIn: function() {
+    this.detectNewSelection(this.get('selected'));
+    // if rendered selection does not equal the new selection, then assume
+    // this is the first 'updateX' called for the new selection.
+    // --> all currently rendered graph elements are now stale.
+    // --> batch 'remove' all elements.
+
+    // whether or not this is the first update call, add nodes and edges for
+    // every member of the result set.
+
+    // after nodes are added, hideNonintersecting()
+    console.log('incoming updated');
+  }.observes('resources.incoming.size').on('init'),
+
+  //this observes the resources list and is invoked on initialization
+  draw: function () {
+
+    var graph = getGraphArrays(this);
+
+    this.cy.load({
+      nodes: graph.nodes,
+      edges: graph.edges
     });
 
-    /*
-    cy.on('tap', 'node.outer', function (evt) {
+    this.hideNonintersecting();
+
+    var swapNames = function (node) {
+      var tmp = node.data("name");
+      node.data("name", node.data("hiddenName"));
+      node.data("hiddenName", tmp);
+    };
+
+    this.cy.on('mouseover', 'node', function (evt) {
+      var node = evt.cyTarget;
+      swapNames(node);
+    });
+
+    this.cy.on('mouseout', 'node', function (evt) {
+      var node = evt.cyTarget;
+      swapNames(node);
+    });
+
+    this.cy.on('tap', 'node.outer', function (evt) {
       var node = evt.cyTarget;
       //TODO detect if value or predicate.  this.resultType?
       //     transition to routes accordingly
       console.log('Outer tap registered: ' + node.id());
     });
 
-    cy.on('tap', 'node.central', function (evt) {
+    this.cy.on('tap', 'node.central', function (evt) {
       var node = evt.cyTarget;
       //TODO transition to what route?  selected resource clicked, so
       // treat the same as click from Things list?
       console.log('Central tap registered: ' + node.id());
     });
-    */
-  }
 
+  }.observes('resources') //TODO observe length of all maps
 });
